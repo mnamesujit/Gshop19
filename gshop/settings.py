ee6c8feb479fec4889d18d03ff5d50eccb3156ed
Sujit kumar <sujit199696@gmail.com>
2021-03-15 23:22:43 +0530

    Fix Bad Request Issue

--- gshop/settings.py
+++ gshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["gshop19.heroku.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
