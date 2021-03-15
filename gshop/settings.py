c128b4143401738f044dee3ad8f3f247c538d9df
Sujit kumar <sujit199696@gmail.com>
2021-03-15 23:14:53 +0530

    Fixing Bad Request 400 Error

--- gshop/settings.py
+++ gshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://git.heroku.com/gshop19.git", "localhost"]
+ALLOWED_HOSTS = ["gshop19.heroku.com", "localhost"]
 
 
 # Application definition
